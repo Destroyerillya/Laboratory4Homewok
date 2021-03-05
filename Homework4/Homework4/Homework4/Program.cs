1db708a460702ba3bcd6fc36e6374b4f34d62721
Destroyer <kasatkinilyaandreevich@gmail.com>
2021-03-06 02:24:41 +0300

    Added lab and homework

---
+++ Homework4/Homework4/Homework4/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Homework4
+{
+    class Program
+    {
+        readonly static CancellationTokenSource _cancelTokenSrc = new CancellationTokenSource();
+        static ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
+        static SqlConnection cnn;
+        static void Main(string[] args)
+        {
+            string connetionString;
+            connetionString = @"Server=.\SQLEXPRESS;Database=blog;Trusted_Connection=True;";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            Console.CancelKeyPress += Console_CancelKeyPress;
+            CancellationToken cancelToken = _cancelTokenSrc.Token;
+            Console.WriteLine("Type commands followed by 'ENTER'");
+            Console.WriteLine("Press CTL+C to Terminate");
+            Console.WriteLine();
+            try
+            {
+                Task.Run(() => DoWork(), cancelToken);
+                Task.Run(() => ListenForInput(), cancelToken);
+                cancelToken.WaitHandle.WaitOne();
+                cancelToken.ThrowIfCancellationRequested();
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Operation Canceled.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        static void ListenForInput()
+        {
+            while (true)
+            {
+                string caseSwitch = Console.ReadLine();
+                switch (caseSwitch)
+                {
+                    case "1":
+                        queue.Enqueue(1);
+                        break;
+                    case "2":
+                        queue.Enqueue(2);
+                        break;
+                    default:
+                        Console.WriteLine("Understand command");
+                        //Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+
+        static void DoWork()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                int result;
+                if (!queue.TryDequeue(out result))
+                {
+                    Console.WriteLine("CQ: TryPeek failed when it should have succeeded");
+                }
+                else if (result != 0)
+                {
+                    if (result == 1)
+                    {
+                        Thread.Sleep(2000);
+                        SqlCommand cmd = new SqlCommand("SELECT * FROM UsersMessages", cnn);
+                        Console.WriteLine(result);
+                        Console.WriteLine(cmd.ExecuteScalar());
+                    }
+                    else
+                    {
+                        Thread.Sleep(3000);
+                        SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM Posts WHERE PublicationTime >= 12/04/2011 12:00:00 AM AND PublicationTime <= 25/05/2011 3:53:04 AM", cnn);
+                        Console.WriteLine(cmd.ExecuteScalar());
+                    }
+                }
+            }
+        }
+
+        static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            cnn.Close();
+            e.Cancel = true;
+            Console.WriteLine("Cancelling...");
+            _cancelTokenSrc.Cancel();
+        }
+    }
+}
---
+++ Laboratory4/Laboratory4/Laboratory4/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Laboratory4
+{
+    class Program
+    {
+        public static int a = 1;
+        static int Sum()
+        { 
+            while (true) 
+            {
+                Thread.Sleep(2000);
+                a += 2;
+            }
+        }
+        static async Task<int> SumAsync()
+        {
+            return await Task.Run(() => Sum());
+        }
+        
+        static int Mul()
+        { 
+            while (true) 
+            {
+                Thread.Sleep(3000);
+                a *= 3;
+            }
+        }
+        static async Task<int> MulAsync()
+        {
+            return await Task.Run(() => Mul());
+        }
+        static int Menu()
+        {   Console.WriteLine("write command show or stop");
+            while (true)
+            {
+                string caseSwitch = Console.ReadLine();
+                
+                switch (caseSwitch)
+                {
+                    case "show":
+                        Console.WriteLine(a);
+                        break;
+                    case "stop":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Understand command");
+                        break;
+                }
+            }
+        }
+        static async Task<int> MenuAsync()
+        {
+            return await Task.Run(() => Menu());
+        }
+        async static Task Main(string[] args)
+        {
+            Task<int> Task1 = SumAsync();
+            Task<int> Task2 = MulAsync();
+            Task<int> Task3 = MenuAsync();
+            int result1 = await Task1;
+            int result2 = await Task2;
+            int result3 = await Task3;
+        }
+    }
+}
